a414916d0f0f15180d88e871a8fb54ca0ead6698
Mathys <mathystheolade973@gmail.com>
2022-03-09 19:11:00 +0100

    update folder

---
+++ App/modeles/Utils/Path.php
@@ -0,0 +1,10 @@
+<?php
+namespace Utils;
+class Path{
+    //<editor-fold desc="Lien interne au site">
+    const CONTROLLER = "App/Controller/";
+    const MODELES = "App/modeles/";
+    const VIEWS = "App/views/";
+    const TEMPLATE = "App/views/default/";
+    //</editor-fold>
+}
